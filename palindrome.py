dee31bae2fc2fa51d582ce44d6f0c7033be4b418
Natanx <natanx.exe@gmail.com>
2016-05-23 22:32:53 +0200

    Script pour définir si un string est un palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,6 @@
+#En partant de l'exercice précédent, écrivez un script qui détermine si une chaîne de caractères donnée est un palindrome
+#(c'est-à-dire une chaîne qui peut se lire indifféremment dans les deux sens), comme par exemple « radar » ou « s.o.s ».
+
 a,b = "anna",""
 L = len(a)
 s = L-1
