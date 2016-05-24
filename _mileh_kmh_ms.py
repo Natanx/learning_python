c2fc0da48b297bdad6baa60b9045e608a6ffa6c7
Natanx <natanx.exe@gmail.com>
2016-05-24 18:30:39 +0200

    Update and rename mileh_kmh_ms.py to _mileh_kmh_ms.py

--- _mileh_kmh_ms.py
+++ _mileh_kmh_ms.py
@@ -1,3 +1,6 @@
+#L'utilisateur entre sa vitesse en miles/h et il s'agit de convertir la valeur en km/h puis en m/s puis de la retourner à 
+#l'utilisateur
+
 mile = input ("Votre vitesse en miles/h : ")
 km = float(mile)*1.609
 ms = km/3.6
