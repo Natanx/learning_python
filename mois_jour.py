5b34b4753ec0d3b910389f470567c96457bcf714
Natanx <natanx.exe@gmail.com>
2016-05-24 18:35:02 +0200

    Update mois_jour.py

--- mois_jour.py
+++ mois_jour.py
@@ -1,3 +1,7 @@
+#Il s'agit à partir de deux listes notés t1 et t2 représentant les mois et leurs nombres de jours respectivement et dans l'ordre
+#d'une année civile et a partir de ces deux listes en créer une troisième qui contiendra en alternance le mois suivit du nombres de 
+#jour qui le compose.
+
 t1 = [31,28,31,30,31,30,31,31,30,31,30,31]
 t2 = [ ' Janvier ' , ' Février ' , ' Mars ' , ' Avril ' , ' Mai ' , ' Juin ' ,
 ' Juillet ' , ' Août ' , ' Septembre ' , 'Octobre ' , ' Novembre ' , ' Décembre ' ]
